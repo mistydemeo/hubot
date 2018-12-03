e80544b2dfd721844d91d90d1a7bcc1584784ee2
Misty De Meo <mistydemeo@github.com>
2018-12-03 18:41:24 +1100

    Datastore: add convenience helpers

--- src/datastore.js
+++ src/datastore.js
@@ -15,6 +15,33 @@ class DataStore {
     return this._set(key, value, "global");
   }
 
+  // Public: Assuming `key` represents an object in the database,
+  // sets its `object_key` to `value`. If `key` isn't already
+  // present, it's instantiated as an empty object.
+  set_object (key, object_key, value) {
+    this.get(key).then((object) => {
+      target = object || {};
+      target[object_key] = value;
+      return this.set(key, value);
+    });
+  }
+
+  // Public: Adds the supplied value(s) to the end of the existing
+  // array in the database marked by `key`. If `key` isn't already
+  // present, it's instantiated as an empty array.
+  set_array (key, value) {
+    this.get(key).then((object) => {
+      target = object || [];
+      // Extend the array if the value is also an array, otherwise
+      // push the single value on the end.
+      if (Array.isArray(value)) {
+        return this.set(key, target.push.apply(target, value));
+      } else {
+        return this.set(key, target.concat(value));
+      }
+    });
+  }
+
   // Public: Get value by key if in the database or return null
   // if not found.
   get (key) {
